ed702fa13b83629fc17e58aeff48716424444481
pritesh-shrivastava <pritesh2312@gmail.com>
2020-04-16 14:42:39 +0530

    [PS] Adding scheme script from local

---
+++ chapter_1/section_1_2/exponentiaton_fast.scm
@@ -0,0 +1,19 @@
+#lang sicp
+;(define (fast-expt b n)
+;  (cond ( (= n 0) 
+;		1
+;	) 
+;	( (is_even n)
+;          (square (fast-expt b (/ n 2)))
+;	)
+;	( else
+;          (* b (fast-expt b (- n 1)))
+;        )
+;	)
+  (define (is_even x)
+    (= (remainder x 2) 0 )
+  )
+  (define (square x)
+    (* x x)
+  )
+;)
