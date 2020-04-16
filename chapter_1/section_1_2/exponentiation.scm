d423dc0a8d0052f2c4b101aacd36dc61c2213205
pritesh-shrivastava <pritesh2312@gmail.com>
2020-04-16 15:22:44 +0530

    [PS] Adding scripts for exponentiation

---
+++ chapter_1/section_1_2/exponentiation.scm
@@ -0,0 +1,24 @@
+#lang sicp
+
+(define (exp_rcrsn b n)
+  (if (= n 0)
+      1
+      (* b (exp_rcrsn b  (- n 1)))
+  )
+)
+
+(define (exp_iter b n)
+  (define (loop counter product)
+    (if (= counter 0)
+        product
+	(loop (- counter 1) (* b product) )
+    )
+  )
+	
+  (loop n 1)
+)
+
+(exp_rcrsn 2 1)
+(exp_iter 2 2)
+(exp_rcrsn 2 3)
+(exp_iter 2 4)
--- chapter_1/section_1_2/exponentiaton_fast.scm
+++ chapter_1/section_1_2/exponentiaton_fast.scm
@@ -1,19 +1,10 @@
 #lang sicp
-;(define (fast-expt b n)
-;  (cond ( (= n 0) 
-;		1
-;	) 
-;	( (is_even n)
-;          (square (fast-expt b (/ n 2)))
-;	)
-;	( else
-;          (* b (fast-expt b (- n 1)))
-;        )
-;	)
-  (define (is_even x)
-    (= (remainder x 2) 0 )
-  )
-  (define (square x)
-    (* x x)
-  )
-;)
+(define (fast-expt b n)
+  (cond ( (= n 0) 1) 
+	( (is_even n) (square (fast-expt b (/ n 2))))
+	( else (* b (fast-expt b (- n 1)))))
+  (define (is_even x) (= (remainder x 2) 0))
+  (define (square x) (* x x))
+)
+
+(fast-expt 2 2)
