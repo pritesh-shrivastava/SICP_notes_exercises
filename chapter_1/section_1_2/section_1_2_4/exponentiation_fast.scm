34482eef66d2e63dbbdbfae47518687ee4856a9c
pritesh-shrivastava <pritesh2312@gmail.com>
2020-05-05 23:23:13 +0530

    [PS] Add solutions for exponentiation

--- chapter_1/section_1_2/section_1_2_4/exponentiation_fast.scm
+++ chapter_1/section_1_2/section_1_2_4/exponentiation_fast.scm
@@ -5,8 +5,8 @@
 
 (define (fast_expt b n)
   (cond ( (= n 0) 1) 
-	;( (is_even n) (square (fast_expt b (/ n 2))))
-	( (is_even n) (fast_expt (square b) (/ n 2)))  ;; Can make this process tail-recursive
+	( (is_even n) (square (fast_expt b (/ n 2))))
+	;( (is_even n) (fast_expt (square b) (/ n 2)))  ;; Can make this process tail-recursive ?? but there seems to be error ??
 	( else (* b (fast_expt b (- n 1))))
   )
 )
---
+++ chapter_1/section_1_2/section_1_2_4/mulitply_fast_using_addition.scm
@@ -0,0 +1,29 @@
+;#lang sicp
+(define (even? x) (= (remainder x 2) 0))
+
+(define (double x) (+ x x)) ; * is not defined in the language
+
+(define (halve x) (/ x 2))
+
+(define (fast_multiply a b)
+	(cond 
+		(
+			(= b 0) 0
+		)
+		(
+			;(even? b) ( (fast_multiply (double a) (halve b) ) ).  ;; causing errors ???
+			(even? b) ( double (fast_multiply a (halve b) ) )
+		)
+		(
+			else (+ a (fast_multiply a (- b 1) ) )
+		)
+	)
+)
+
+(halve 2)
+(double 1)
+(fast_multiply 2 1)
+(fast_multiply 2 2)
+(fast_multiply 3 7)
+(fast_multiply 2 9)
+(fast_multiply 4 8)
