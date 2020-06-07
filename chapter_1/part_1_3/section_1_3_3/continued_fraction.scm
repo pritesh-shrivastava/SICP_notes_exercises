ffa4dec418314ebd06a07aa8ad48bcdbd08ca7b4
pritesh-shrivastava <pritesh2312@gmail.com>
2020-06-07 20:07:40 +0530

    [PS] On Ex1.37

---
+++ chapter_1/part_1_3/section_1_3_3/continued_fraction.scm
@@ -0,0 +1,18 @@
+
+(define (cont-frac n d k)
+	
+
+
+
+
+
+
+
+
+
+
+;; Check by approximating the golden ratio by passing n & d to return 1 always
+(cont-frac (lambda (i) 1.0)
+           (lambda (i) 1.0)
+           k)
+
