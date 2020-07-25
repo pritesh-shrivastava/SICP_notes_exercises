c8662a03270a57b2ce9ee1ab02fa64bcde36ac6e
pritesh-shrivastava <pritesh2312@gmail.com>
2020-07-25 14:25:08 +0530

    [PS] On Ex2.9

---
+++ chapter_2/part_2_1/section_2_1_4/interval_arithmetic.scm
@@ -0,0 +1,81 @@
+;; Interval abstraction
+;;; Interval constructor
+(define (make-interval a b) (cons a b))
+
+;;; Interval selectors
+
+;; If upper and lower bounds are provided to the constructor in order :
+; (define (lower-bound i) (car i))
+; (define (upper-bound i) (cdr i))
+;; However, we are not validatiing user input in the construcor, hence,
+;; this is safer
+(define (upper-bound i) (max (car i) (cdr i))) 
+(define (lower-bound i) (min (car i) (cdr i))) 
+
+;; Operations on intervals
+
+;;; Addition
+(define (add-interval x y)
+    (make-interval  (+  (lower-bound x) 
+                        (lower-bound y))
+                    (+  (upper-bound x) 
+                        (upper-bound y))
+    )
+)
+
+;;; Multiplication of intervals
+(define (mul-interval x y)
+    (let 
+        (
+            (p1 (* (lower-bound x) 
+                   (lower-bound y)))
+            (p2 (* (lower-bound x) 
+                   (upper-bound y)))
+            (p3 (* (upper-bound x) 
+                   (lower-bound y)))
+            (p4 (* (upper-bound x) 
+                   (upper-bound y)))
+        )
+        (   make-interval   (min p1 p2 p3 p4)
+                            (max p1 p2 p3 p4)
+        )
+    )
+)
+
+;;; Division of intervals
+(define (div-interval x y)
+    (mul-interval   x 
+                    (make-interval 
+                        (/ 1.0 (upper-bound y)) 
+                        (/ 1.0 (lower-bound y))
+                    )
+    )
+)
+
+;;; Substraction of intervals
+(define (sub-interval x y)
+    (make-interval  (-  (lower-bound x) 
+                        (upper-bound y))
+                    (-  (upper-bound x) 
+                        (lower-bound y))
+    )
+)
+
+;;; For testing
+;;; Print interval
+(define (display-interval i) 
+   (newline) 
+   (display "[") 
+   (display (lower-bound i)) 
+   (display ",") 
+   (display (upper-bound i)) 
+   (display "]")
+)
+
+;; Testing 
+ (define i (make-interval 2 7)) 
+ (define j (make-interval 8 3)) 
+  
+ (display-interval i) 
+ (display-interval (sub-interval i j)) 
+ (display-interval (sub-interval j i)) 
--- chapter_2/part_2_1/section_2_1_4/section_2_1_4.md
+++ chapter_2/part_2_1/section_2_1_4/section_2_1_4.md
@@ -1,9 +1,25 @@
-#### Ex 2.7
+#### Ex2.7
 
 Alyssa’s program is incomplete because she has not specified the implementation of the interval abstraction. Here is a definition of the interval constructor:
 ```
 (define (make-interval a b) (cons a b))
 ```
-Define selectors upper-bound and lower-bound to complete the implementation.
+Define selectors `upper-bound` and `lower-bound` to complete the implementation.
+
+_Solution_ : Check script _interval_arithmetic.scm_ .
+
+
+#### Ex2.8
+
+Using reasoning analogous to Alyssa’s, describe how the difference of two intervals may be computed. Define a corresponding subtraction procedure, called sub-interval.
+
+_Solution_ : Check script _interval_arithmetic.scm_ .
+
+
+#### Ex2.9
+
+The width of an interval is half of the difference between its upper and lower bounds. The width is a measure of the uncertainty of the number specified by the interval. For some arithmetic operations the width of the result of combining two intervals is a function only of the widths of the argument intervals, whereas for others the width of the combination is not a function of the widths of the argument intervals. 
+Show that the width of the sum (or difference) of two intervals is a function only of the widths of the intervals being added (or subtracted. 
+Give examples to show that this is not true for multiplication or division. 
 
 _Solution_ : 
